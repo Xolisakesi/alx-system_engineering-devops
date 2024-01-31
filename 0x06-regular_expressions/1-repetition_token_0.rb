d8429ec12a1c41e01c28fce586e2bd9253114de6
Xolisakesi <xolisakesi@gmail.com>
2024-01-31 22:59:28 +0200

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,4 @@
 #!/usr/bin/env ruby
 
-def match_school(string)
-  regex = /School/
-  if string.match?(regex)
-    puts string.gsub(regex, 'School')
-  else
-    puts ''
-  end
-end
-
-match_school(ARGV[0])
+puts ARGV[0].scan(/hbt{2,5}n/).join
 
