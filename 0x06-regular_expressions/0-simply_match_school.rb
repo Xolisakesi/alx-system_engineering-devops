80c8f916859542de1a20d1585097b941582b3c58
Xolisakesi <xolisakesi@gmail.com>
2024-01-31 21:48:23 +0200

    0-simply_match_school.rb

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+def match_school(string)
+  regex = /School/
+  if string.match?(regex)
+    puts string.gsub(regex, 'School')
+  else
+    puts ''
+  end
+end
+
+match_school(ARGV[0])
