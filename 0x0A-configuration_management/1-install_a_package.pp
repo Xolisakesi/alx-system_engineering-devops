02d9b4c2e10d8ae8387790295697e47c8516ac47
Xolisakesi <xolisakesi@gmail.com>
2024-03-25 13:23:11 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
 }
